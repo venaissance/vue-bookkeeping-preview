f00068f012ad659d3ad31b83a975390e8a4805f2
varian <mizeyuyu@gmail.com>
2020-07-22 17:20:26 +0800

    deploy

---
+++ precache-manifest.f9e89e8919d8d4447acbdb6d8427b183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d4736f518ee72e0096",
+    "url": "/vue-bookkeeping-preview/css/app.3cc85d87.css"
+  },
+  {
+    "revision": "77c3641556e3f4aee1509d2570b8a997",
+    "url": "/vue-bookkeeping-preview/index.html"
+  },
+  {
+    "revision": "22d4736f518ee72e0096",
+    "url": "/vue-bookkeeping-preview/js/app.a051e2eb.js"
+  },
+  {
+    "revision": "2a2196ec0b6944c8a427",
+    "url": "/vue-bookkeeping-preview/js/chunk-vendors.9445aee8.js"
+  },
+  {
+    "revision": "821897dec905b1732d6cba0a04dfba53",
+    "url": "/vue-bookkeeping-preview/manifest.json"
+  },
+  {
+    "revision": "1d9a37fcc0e5164cb10d18450c83664b",
+    "url": "/vue-bookkeeping-preview/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-bookkeeping-preview/robots.txt"
+  }
+]);
